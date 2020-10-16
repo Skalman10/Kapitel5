11a7e92e64580d8d2bb7bf27a4174b4d10e711a9
Skalmanleif <isak.thyrel@elev.ga.ntig.se>
2020-10-16 11:06:39 +0200

    Hej

--- src/upprepa.java
+++ src/upprepa.java
@@ -3,5 +3,7 @@ import javax.swing.*;
 public class upprepa {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "test");
+        JOptionPane.showMessageDialog(null, "Hej");
+
     }
 }
